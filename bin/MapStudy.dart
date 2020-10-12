f9907c627b32cf5eb80f475e8299d7149954f5ea
Youhei Oonuma <oonuma617@gmail.com>
2020-10-12 14:15:12 +0900

    map

--- bin/MapStudy.dart
+++ bin/MapStudy.dart
@@ -3,8 +3,16 @@ void main() {
   final map = {
     'a': 1,
     'b': 2,
+    'c': 3,
   };
 
+  // mapから直接mapする
   print(map.map((key, value) => MapEntry(value, key)));
+
+  // mapから一旦entriesを挟む
   print(map.entries.map((e) => e.value));
+
+  map.forEach((key, value) {
+    print('key:$key value:$value');
+  });
 }
